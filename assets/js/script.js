2890dba78b8b5312591ac91a5e0fe776d1f6b73a
Damerian Dark <newbmailz@gmail.com>
2021-08-03 16:11:16 -0500

    setup visiblity for the form and set up variables as well as first function to check for length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let ques = document.querySelector("#question");
+let uI = document.querySelector("#userInput");
+let vE = document.querySelector("#validationError");
+let enter = document.querySelector("#enter");
+let yesBtn = document.querySelector("#yesBtn");
+let noBtn = document.querySelector("#noBtn");
+let sP = document.querySelector("#settingsPrompt");
+let passLength = 0;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  sP.style.visibility = "visible";
+  yesBtn.style.visibility = "hidden";
+  noBtn.style.visibility = "hidden";
+   enter.addEventListener ("click" , changeLength);
+  
+  for (let i = 0; i < passLength; i++) {
+    const element = array[i];
+    
+  }
 
-  passwordText.value = password;
+  // let password = generatePassword();
+  // let passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
+function changeLength(){
+  
+  passLength = uI.value;
+  console.log(passLength);
+  // if () 
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
